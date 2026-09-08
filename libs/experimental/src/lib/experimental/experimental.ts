c3b380972b58933e057604cdd30e3a102b1903c4
Aleksey Lastovka <lastovkaalexei@yandex.ru>
2026-09-08 09:51:00 +0700

    Пример реализации отписки.

--- libs/experimental/src/lib/experimental/experimental.ts
+++ libs/experimental/src/lib/experimental/experimental.ts
@@ -1,21 +1,69 @@
-import { Component, signal, WritableSignal } from '@angular/core';
-import { NgComponentOutlet } from '@angular/common';
-import { RandomNumComponent } from './random-num/random-num.component';
+import { afterNextRender, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { map, scan, takeUntil, timer } from 'rxjs';
+import { DestroyService } from './destroy.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+function factorialize(num: number): number {
+  if (!Number.isInteger(num) || num < 0) {
+    throw new TypeError('Нужно передать неотрицательное целое число');
+  }
+
+  let result = 1;
+
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+}
 
 @Component({
   selector: 'lib-experimental',
-  imports: [NgComponentOutlet, RouterLink],
+  imports: [RouterLink],
   templateUrl: './experimental.html',
   styleUrl: './experimental.css',
+  host: { ngSkipHydration: 'true' },
+  providers: [DestroyService],
 })
 export class ExperimentalComponent {
-  protected readonly lazeComponent: WritableSignal<typeof RandomNumComponent | null> = signal<
-    typeof RandomNumComponent | null
-  >(null);
-  protected loadComponent(): void {
-    import('./random-num/random-num.component').then((c): void => {
-      this.lazeComponent.set(c.RandomNumComponent);
-    });
+  subNumber = 0;
+
+  state: Record<string, number[]> = {};
+  destroy$ = inject(DestroyService);
+
+  constructor() {
+    timer(0, 100)
+      .pipe(
+        map((val) => {
+          return factorialize(val * 10);
+        }),
+        scan((acc, curr: number) => {
+          return acc + curr;
+        }, 0),
+        takeUntilDestroyed(),
+      )
+      .subscribe((val) => {
+        console.log(123123);
+      });
+  }
+
+  addSub() {
+    this.subNumber++;
+    this.state[this.subNumber + ''] = [];
+    timer(0, 100)
+      .pipe(
+        map((val) => {
+          return factorialize(val * 10);
+        }),
+        scan((acc, curr: number) => {
+          return acc + curr;
+        }, 0),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((val) => {
+        this.state[this.subNumber + ''].push(val);
+        console.log(this.state);
+      });
   }
 }
